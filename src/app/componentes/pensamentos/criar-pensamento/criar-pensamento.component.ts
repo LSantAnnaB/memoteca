bf0ac9c736725d12ddd396ed58ffd2c5b5df6d6b
Lucas Sant'Anna <98597613+LSantAnnaB@users.noreply.github.com>
2023-06-19 19:20:37 -0300

    MemoTeca

---
+++ src/app/componentes/pensamentos/criar-pensamento/criar-pensamento.component.ts
@@ -0,0 +1,41 @@
+import { Router } from '@angular/router';
+import { PensamentoService } from './../pensamento.service';
+import { Pensamento } from './../pensamento';
+import { Component, OnInit } from '@angular/core';
+import { FormGroup } from '@angular/forms';
+
+@Component({
+  selector: 'app-criar-pensamento',
+  templateUrl: './criar-pensamento.component.html',
+  styleUrls: ['./criar-pensamento.component.css']
+})
+export class CriarPensamentoComponent implements OnInit {
+
+  pensamento: Pensamento = {
+
+    conteudo: '',
+    autoria: '',
+    modelo: 'modelo1'
+  }
+
+  formulario!: FormGroup
+
+  constructor(
+    private service: PensamentoService,
+    private router: Router
+  ) { }
+
+  ngOnInit(): void {
+  }
+
+  criarPensamento() {
+    this.service.criar(this.pensamento).subscribe(() => {
+      this.router.navigate(['/listarPensamento'])
+    })
+  }
+
+  cancelar() {
+    this.router.navigate(['/listarPensamento'])
+  }
+
+}
