14bf27a11a0ce4cfd1955bc5beded6bdd56aad7b
Lucas Sant'Anna <98597613+LSantAnnaB@users.noreply.github.com>
2023-06-20 18:01:49 -0300

    Add formulario

--- src/app/componentes/pensamentos/criar-pensamento/criar-pensamento.component.ts
+++ src/app/componentes/pensamentos/criar-pensamento/criar-pensamento.component.ts
@@ -2,7 +2,7 @@ import { Router } from '@angular/router';
 import { PensamentoService } from './../pensamento.service';
 import { Pensamento } from './../pensamento';
 import { Component, OnInit } from '@angular/core';
-import { FormGroup } from '@angular/forms';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-criar-pensamento',
@@ -11,31 +11,51 @@ import { FormGroup } from '@angular/forms';
 })
 export class CriarPensamentoComponent implements OnInit {
 
-  pensamento: Pensamento = {
 
-    conteudo: '',
-    autoria: '',
-    modelo: 'modelo1'
-  }
+
 
   formulario!: FormGroup
 
   constructor(
     private service: PensamentoService,
-    private router: Router
+    private router: Router,
+    private formBuilder: FormBuilder
   ) { }
 
   ngOnInit(): void {
+    this.formulario = this.formBuilder.group({
+      conteudo: ['',Validators.compose([
+        Validators.required,
+      Validators.pattern(/(.|\s)*\S(.|\s)*/)
+    ])],
+      autoria: ['',Validators.compose([
+        Validators.required,
+      Validators.minLength(3)
+    ])],
+      modelo: ['modelo1', [Validators.required]]
+    })
   }
 
   criarPensamento() {
-    this.service.criar(this.pensamento).subscribe(() => {
-      this.router.navigate(['/listarPensamento'])
-    })
+    console.log(this.formulario.get('autoria')?.errors)
+    if (this.formulario.valid) {
+      this.service.criar(this.formulario.value).subscribe(() => {
+        this.router.navigate(['/listarPensamento'])
+      })
+    }
   }
 
   cancelar() {
     this.router.navigate(['/listarPensamento'])
+  }
+
+  habilitarBotao():string {
+    if (this.formulario.valid) {
+      return 'botao'
+
+    } else{
+      return ' botao_desabilitado'
+    }
   }
 
 }
